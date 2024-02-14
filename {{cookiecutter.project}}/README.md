9aaba2401a8309824933a533ed6dfad986f91fd2
Alvaro Albarran <alvaro.albarran@turingchallenge.com>
2024-02-14 10:04:17 +0100

    feat: Updated README, docs for config, etc

--- {{cookiecutter.project}}/README.md
+++ {{cookiecutter.project}}/README.md
@@ -5,34 +5,56 @@
 # {{cookiecutter.project}}
 -----------------------------
 # Checklist
-- [ ] Leer Oferta del proyect
+- [ ] Leer Oferta del proyecto
 - [ ] Añadir el remote del devops a git
 - [ ] Revisar scope con {{cookiecutter.owner}} y PM
 - [ ] Revisar Diseño y Arquitectura con {{cookiecutter.owner}} y PM
+- [ ] En el caso de usar dvc, crear un blob_storage y añadir el connection_string al .env
 # Introduction
 
-# Set-Up
-Be sure to have poetry and the required python version running(pyenv can help with this).  
+# Getting Started
+### Requirements
+#### Docker
+Docker must be installed.
+#### Make
+For all the referenced `make` commands, it has to be installed, for unix systems
+its installed by defaults, in windows you can use winget(`winget install GnuWin32.Make`) or choco(`choco install make`) for example.
+If you dont want to install `make` you can look up the corresponding commands on the `Makefile`.
+
+
+### Using poetry
+Be sure to have poetry and the required python version running.
 For creating the environment and installing the dependencies
-run `make install`.  
-Be sure to activate the virtual environment, vscode will promp you to use the   detected venv, just open a new terminal window, it should be activated.  
-For manual activation run `poetry shell`.  
-As a temporal fix, use `make temp_install` to create a virtual environment
-using the requirements_dev.txt.  
-For using dvc the remote and connection_string must be added  
-for dvc:
-> dvc remote add -d blob_origin azure://mycontainer/path
+run `make install`.
+Be sure to activate the virtual environment, vscode will promp you to use the detected venv,
+just open a new terminal window, it should be activated.
+For manual activation run `poetry shell`.
+### Using venv
+If you preffer using venv, use `make venv_install` to create a virtual environment
+using the requirements_dev.txt.
+### Using devcontainers
+Create the container using `make amd_container` or `make arm_container` if you are using a arm architecture.
+When the image is build you can run the container with `make run_amd_container` or `make run_arm_container` respectively.
+Alternatively you can download already generated images with `make data`.
 
-Add blob connection string:
-> dvc remote modify --local blob_origin connection_string 'mysecret'
+## DVC
+For using dvc you first have to set it up: `make dvc_setup`
 
-For more information on dvc data-management : [https://dvc.org/doc/start/data-management/data-versioning](dvc-data-versioning)  
+For more information on dvc data-management : [https://dvc.org/doc/start/data-management/data-versioning](dvc-data-versioning)
 After dvc is setup just run `make data`  to download the dvc tracked data.
 
 ## Credentials
-* The dvc connection_string for accesing the dvc tracked files.
-* `.env` file ??
+* The `.env` has all the necesary credentials related to the following services:
+    * DVC
+    * .env encryption key
 
+For decrypting the `.env.enc` you need at least the openssl key on you .env.
+You can update/rewrite your .env decrypting this file with `make decrypt_env`
+
+# Deployment
+For deploying the container, it can be automatically created and uploaded to the ACI(if the appropiate credentials are on the .env) with `make push_amd_container`.  
+If you just want to create the image: `make amd_container`.  
+In case an arm architecture is needed: `make arm_container`.  
 
 ## Runs
 How to run ?
--- {{cookiecutter.project}}/src/config/config.py
+++ {{cookiecutter.project}}/src/config/config.py
@@ -1,24 +1,56 @@
+"""
+App Configuration Management Module
+Overview
+This module manages application configurations, utilizing dataclasses for defining configuration 
+models and supporting environment-specific settings through YAML files.
 
+Configuration Files
+- Development: Uses config_dev.yaml by default.
+- Production: Switches to config_prod.yaml when ENVIRONMENT is set to 'PROD'.
+
+Environment Variables Override
+Override configuration using environment variables in .env file or as exported variables.
+
+Naming convention:
+[ENV]_[CONFIG_SECTION]_[CONFIG_KEY]. For example: DEV_SECRETS_API_KEY.
+[ENV]_[CONFIG_KEY]. if it doesnt have section. For example: DEV_LOG_LEVEL 
+Note: Always all in caps
+
+Usage
+- Define configuration settings in the YAML files.
+- Optionally override specific settings via environment variables.
+- Access configurations through the Config, Secrets, and Model classes.
+"""
 from __future__ import annotations
+
 import os
 import pathlib
 from dataclasses import dataclass
+from typing import Literal
 
 import yaml
 from dacite import from_dict
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+ENVIRONMENT = os.environ["ENVIRONMENT"]
+config_filename = 'config_prod.yaml' if ENVIRONMENT == "PROD" else 'config_dev.yaml'
 
 
 # ----------------- EXAMPLE -----------------
 @dataclass
 class Config:
-    files: Files
+    secrets: Secrets
     model: Model
+    log_level: Literal["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]
+    environment: Literal["PROD", "DEV"]
 
 
-@dataclass
-class Files:
-    train_data: str
-    test_data: str
+@dataclass(repr=False)
+class Secrets:
+    api_key: str
     
 
 @dataclass
@@ -34,21 +66,47 @@ with open(CONFIG_PATH, 'r') as file:
     yaml_config = yaml.safe_load(file)
 
 
+def get_env_var_value(env_var_name: str,
+                      environment: Literal['DEV', 'PROD'] | None=None
+                      ) -> int | str | None:
+    if environment:
+        env_var_name = environment + '_' + env_var_name
+
+    env_var_value = None
+    if env_var_name in os.environ:
+        try:
+            env_var_value = int(os.environ[env_var_name])
+        except ValueError:
+            env_var_value = os.environ[env_var_name]
+    return env_var_value
+
 # Iterate through the keys of yaml_config to check if the corresponding env variable exists
 for section_key, section_value in yaml_config.items():
+
+    # First level
+    if not isinstance(section_value, dict):
+        env_var_name = f"{section_key.upper()}"
+        env_var_value = get_env_var_value(env_var_name)
+        if env_var_value:
+            yaml_config[section_key] = env_var_value
+        else:
+            env_var_value = get_env_var_value(env_var_name,
+                                              environment=yaml_config["environment"])
+            if env_var_value:
+                yaml_config[section_key] = env_var_value
+        continue
+
+    # Second Level
     for key in section_value:
         env_var_name = f"{section_key.upper()}_{key.upper()}"
-        if env_var_name in os.environ:
-            # TODO: Improve type casting from env variables
-            try:
-                env_var_value = int(os.environ[env_var_name])
-            except ValueError:
-                env_var_value = os.environ[env_var_name]
-
+        env_var_value = get_env_var_value(env_var_name)
+        if env_var_value:
             yaml_config[section_key][key] = env_var_value
+        else:
+            env_var_value = get_env_var_value(env_var_name,
+                                              environment=yaml_config["environment"])
+            if env_var_value:
+                yaml_config[section_key][key] = env_var_value
 
-# Write the modified dictionary back to the config.yaml file
-with open(CONFIG_PATH, 'w') as file:
-    yaml.dump(yaml_config, file)
 
 config: Config = from_dict(data_class=Config, data=yaml_config)
